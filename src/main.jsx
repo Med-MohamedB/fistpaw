3b7df88114f1c3d34a907ded2d46e8de19abaf4c
Benia Mohamed <mohamed.benia@univ-tiaret.dz>
2026-07-07 22:07:30 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FistPaw from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FistPaw />
+  </React.StrictMode>
+);
